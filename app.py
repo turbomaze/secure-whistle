10b229a9f0a30497ba597debe5450cdb51a94476
Anthony Liu <anthonyscottliu@gmail.com>
2018-05-12 18:57:07 -0400

    More code.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import random
+import requests
 from struct import pack
 from hashlib import sha256 as H
 from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+token_endpoint='https://oidc.mit.edu/authorize'
 valid_states = set()
 
 def get_random_nonce():
@@ -26,6 +28,8 @@ def authorize():
     url = 'https://oidc.mit.edu/authorize?'
     url += 'client_id=' + os.environ['CLIENT_ID']
     url += '&response_type=code'
+    url += '&scope=openid'
+    url += '&redirect_uri=https://confess.anthony.ai/login'
     url += '&state=' + state
     url += '&nonce=' + nonce
     valid_states.add(state)
@@ -38,6 +42,18 @@ def login():
     if state in valid_states:
         # valid, continue authorizing
         valid_states.remove(state)
+
+        # get the token
+        foo = requests.post(
+            token_endpoint,
+            auth=(os.environ['CLIENT_ID'], os.environ['CLIENT_SECRET']),
+            data={
+              'grant_type': 'authorization_code',
+              'code': code,
+              'redirect_uri': 'https://confess.anthony.ai/login'
+            }
+        )
+        print foo
         return render_template('login.html')
     else:
         # not valid, reject user
