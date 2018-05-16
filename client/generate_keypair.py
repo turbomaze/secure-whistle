343f0914389427717880b1b09c1b34e98b485756
Anthony Liu <anthonyscottliu@gmail.com>
2018-05-16 02:44:57 -0400

    Change pub/priv key format.

--- client/generate_keypair.py
+++ client/generate_keypair.py
@@ -3,15 +3,16 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(__file__ + '/../../ecc_linkable_ring_signatures'))
-from ecdsa import SigningKey, SECP256k1
+from ecdsa import SECP256k1
+from ecdsa.util import randrange
 
 if len(sys.argv) <= 1:
     sys.exit(0)
 
 file_name_prefix = sys.argv[1]
 
-sk = SigningKey.generate(curve=SECP256k1)
-vk = sk.get_verifying_key()
+sk = randrange(SECP256k1.order)
+pk = SECP256k1.generator * sk
 
-open(file_name_prefix + '_private.pem', 'wb').write(sk.to_pem())
-open(file_name_prefix + '_public.pem', 'wb').write(vk.to_pem())
+open(file_name_prefix + '_private.int', 'wb').write(str(sk).encode())
+open(file_name_prefix + '_public.int', 'wb').write(str(pk).encode())
