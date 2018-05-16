261cacdefc7dd573fa87f8be0c1615bdd6dc5eab
Anthony Liu <anthonyscottliu@gmail.com>
2018-05-15 22:33:36 -0400

    Add scripts to send signed pub keys to ledger.

--- client/add_key_to_ledger.py
+++ client/add_key_to_ledger.py
@@ -3,8 +3,9 @@
 import os
 import sys
 import requests
+import base64
 from hashlib import sha256 as H
-from ecdsa import SigningKey, NIST256p
+from ecdsa import SigningKey
 
 base_url = 'https://confess.anthony.ai'
 
@@ -21,7 +22,13 @@ elif not os.path.isfile(signing_key_file):
     print 'could not find file at: %s' % signing_key_file
     sys.exit(0)
 
-public_key = open(public_key_file, 'rb').read()
+public_key = open(public_key_file, 'r').read()
 signing_key = SigningKey.from_pem(open(signing_key_file).read())
 signature = signing_key.sign(public_key)
-open('my_signature', 'w').write(signature)
+open('my_signature', 'wb').write(signature)
+
+resp = requests.post(base_url + '/ledger/add', data={
+    'pub_key': public_key,
+    'sig': base64.b64encode(signature)
+})
+print resp.json()
---
+++ client/generate_keypair.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+from ecdsa import SigningKey, NIST256p
+
+if len(sys.argv) <= 1:
+    sys.exit(0)
+
+file_name_prefix = sys.argv[1]
+
+sk = SigningKey.generate(curve=NIST256p)
+vk = sk.get_verifying_key()
+
+open(file_name_prefix + '_private.pem', 'w').write(sk.to_pem())
+open(file_name_prefix + '_public.pem', 'w').write(vk.to_pem())
