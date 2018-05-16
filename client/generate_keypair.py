ef36b29004807d8f7bb845c1de6ae27cf6c96d39
Anthony Liu <anthonyscottliu@gmail.com>
2018-05-16 01:55:17 -0400

    Use the secp256k1 curve instead.

--- client/generate_keypair.py
+++ client/generate_keypair.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath(__file__ + '/../../ecc_linkable_ring_signatures'))
-from ecdsa import SigningKey, NIST256p
+from ecdsa import SigningKey, SECP256k1
 
 if len(sys.argv) <= 1:
     sys.exit(0)
 
 file_name_prefix = sys.argv[1]
 
-sk = SigningKey.generate(curve=NIST256p)
+sk = SigningKey.generate(curve=SECP256k1)
 vk = sk.get_verifying_key()
 
-open(file_name_prefix + '_private.pem', 'w').write(sk.to_pem())
-open(file_name_prefix + '_public.pem', 'w').write(vk.to_pem())
+open(file_name_prefix + '_private.pem', 'wb').write(sk.to_pem())
+open(file_name_prefix + '_public.pem', 'wb').write(vk.to_pem())
